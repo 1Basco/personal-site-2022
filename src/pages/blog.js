447865198baba55b642f3d132f23006f8a37c56d
1Basco <lsos13@gmail.com>
2023-01-27 19:21:50 -0300

    [Disable blog](Remove some code to deploy)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,27 +5,27 @@ import { Section } from "../styles/GlobalComponents";
 import useSWR from "swr";
 import PostList from "../components/PostList/PostList";
 
-async function fetcher(...args) {
-  const response = await fetch(...args);
-  const json = await response.json();
-  return setPosts(json);
-}
+// async function fetcher(...args) {
+//   const response = await fetch(...args);
+//   const json = await response.json();
+//   return setPosts(json);
+// }
 
 const Blog = () => {
   // get the posts via the api blog_posts
-  const [posts, setPosts] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const { data } = useSWR("/api/blog_posts", fetcher);
+  // const [posts, setPosts] = useState({});
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(false);
+  // const { data } = useSWR("/api/blog_posts", fetcher);
 
   return (
     <Layout>
       <Section grid>
         <Hero />
       </Section>
-      <Section>
+      {/* <Section>
         <PostList posts={posts} />
-      </Section>
+      </Section> */}
     </Layout>
   );
 };
